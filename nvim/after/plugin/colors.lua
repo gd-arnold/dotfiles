f7d84a3cd651c07d1e09ae25d0738acc0fecc0ef
GeorgiArn <georgearnaudov1@gmail.com>
2023-04-17 23:55:14 +0300

    nvim: change colorscheme to gruvbox

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -1,14 +1,3 @@
-require('rose-pine').setup({
-    disable_background = true
-})
+require('gruvbox')
 
-function ColorMyPencils(color)
-	color = color or "rose-pine"
-	vim.cmd.colorscheme(color)
-
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-
-end
-
-ColorMyPencils()
+vim.cmd.colorscheme('gruvbox');
