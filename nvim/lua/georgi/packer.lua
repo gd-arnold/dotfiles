ba3e384c8a65f2624ec4e0449dc8497084aa0ad5
GeorgiArn <georgearnaudov1@gmail.com>
2023-04-18 00:06:50 +0300

    nvim: install tmux navigator

--- nvim/lua/georgi/packer.lua
+++ nvim/lua/georgi/packer.lua
@@ -62,5 +62,5 @@ return require('packer').startup(function(use)
     use("github/copilot.vim")
     use("eandrju/cellular-automaton.nvim")
     use("laytan/cloak.nvim")
-
+    use("christoomey/vim-tmux-navigator")
 end)
