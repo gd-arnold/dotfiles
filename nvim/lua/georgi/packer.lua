f87aac78ddb4d13d677de29c7e0c01dc02fe1961
GeorgiArn <georgearnaudov1@gmail.com>
2023-05-23 19:37:56 +0300

    nvim: markdown preview plugin

--- nvim/lua/georgi/packer.lua
+++ nvim/lua/georgi/packer.lua
@@ -71,4 +71,15 @@ return require('packer').startup(function(use)
         opt = true,
         run = "npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out"
     }
+
+    -- Markdown
+    use({
+        "iamcco/markdown-preview.nvim",
+        run = function() vim.fn["mkdp#util#install"]() end,
+    })
+    use({
+        "iamcco/markdown-preview.nvim", run = "cd app && npm install",
+        setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" },
+    })
+
 end)
