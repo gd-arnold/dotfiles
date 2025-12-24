47c582e1c4d6207a7a3f16f390c14053d27fbdc5
gd-arnold <gdarnold19@gmail.com>
2025-12-25 01:27:27 +0200

    nvim: forward yanking to windows clipboard

--- nvim/init.lua
+++ nvim/init.lua
@@ -223,3 +223,18 @@ vim.opt.isfname:append("@-@")
 vim.opt.updatetime = 50
 
 vim.opt.colorcolumn = "80"
+
+-- WSL2: use Windows clipboard via win32yank
+vim.opt.clipboard = "unnamedplus"
+vim.g.clipboard = {
+  name = "win32yank",
+  copy = {
+    ["+"] = "win32yank.exe -i --crlf",
+    ["*"] = "win32yank.exe -i --crlf",
+  },
+  paste = {
+    ["+"] = "win32yank.exe -o --lf",
+    ["*"] = "win32yank.exe -o --lf",
+  },
+  cache_enabled = 0,
+}
